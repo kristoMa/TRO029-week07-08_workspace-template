41bfed5d15d79a383f1881463ee9287bc35ffb08
kristoMa <kristo.magi@tktk.ee>
2026-08-24 17:54:50 +0000

    fix: week7-8ex

--- ros2_ws/src/week07_08_my_package/week07_08_my_package/my_node.py
+++ ros2_ws/src/week07_08_my_package/week07_08_my_package/my_node.py
@@ -1,6 +1,14 @@
-def main():
-    print('Hi from week07_08_my_package.')
+import time
 
+
+def main():
+    print('Hi from week07_08_my_package.', flush=True)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
